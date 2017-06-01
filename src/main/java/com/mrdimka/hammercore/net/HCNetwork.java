9beb6244a0be0cd18882dc33d443b9e4b7928d44
MrDimka's Studio <mrdimka@yahoo.com>
2017-06-01 15:35:42 +0300

    reverted to single bus

--- src/main/java/com/mrdimka/hammercore/net/HCNetwork.java
+++ src/main/java/com/mrdimka/hammercore/net/HCNetwork.java
@@ -1,28 +1,14 @@
 package com.mrdimka.hammercore.net;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import com.mrdimka.hammercore.HammerCore;
 import com.mrdimka.hammercore.net.packetAPI.PacketManager;
 
 public class HCNetwork
 {
 	public static final PacketManager manager = new PacketManager("hammercore");
 	
-	private static final Map<String, PacketManager> managers = new HashMap<>();
-	
-	static
-	{
-		for(String key : HammerCore.initHCChannels)
-			managers.put(key, new PacketManager("custom" + key));
-	}
-	
 	public static PacketManager getManager(String name)
 	{
-		if(name == null || name.equals("hammercore"))
-			return manager;
-		return managers.get(name);
+		return manager;
 	}
 	
 	public static void clinit()
