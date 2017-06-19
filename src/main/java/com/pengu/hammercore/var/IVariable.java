92f65d0f0a1e36b9f9c23ded281d88dc281e7328
MrDimka's Studio <mrdimka@yahoo.com>
2017-06-19 13:41:01 +0300

    Update to 1.8.7

---
+++ src/main/java/com/pengu/hammercore/var/IVariable.java
@@ -0,0 +1,14 @@
+package com.pengu.hammercore.var;
+
+import net.minecraft.nbt.NBTTagCompound;
+
+public interface IVariable<T>
+{
+	T get();
+	void set(T t);
+	NBTTagCompound writeToNBT(NBTTagCompound nbt);
+	void readFromNBT(NBTTagCompound nbt);
+	boolean hasChanged();
+	void setNotChanged();
+	String getId();
+}
---
+++ src/main/java/com/pengu/hammercore/var/VarEvents.java
@@ -0,0 +1,25 @@
+package com.pengu.hammercore.var;
+
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.ServerTickEvent;
+
+import com.mrdimka.hammercore.annotations.MCFBus;
+
+@MCFBus
+public class VarEvents
+{
+	@SubscribeEvent
+	public void serverTick(ServerTickEvent evt)
+	{
+		VariableManager.updateManager();
+	}
+	
+	@SubscribeEvent
+	public void playerJoin(PlayerLoggedInEvent evt)
+	{
+		if(evt.player instanceof EntityPlayerMP)
+			VariableManager.sendVarsTo((EntityPlayerMP) evt.player);
+	}
+}
